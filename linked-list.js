e5723e4e3e0c5dd28e233fdeb571eb1ff28ebe66
Ashley Mathew <amathew195@gmail.com>
2022-12-12 14:33:41 -0600

    completed up to shift

--- linked-list.js
+++ linked-list.js
@@ -23,26 +23,71 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
+    let newNode = new Node(val);
 
+    if (this.head === null) this.head = newNode;
+
+    if (this.tail !== null) this.tail.next = newNode;
+
+    this.tail = newNode;
+    this.length++;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
+    let newNode = new Node(val);
 
+    if (this.head) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+    if (this.tail) {
+      let current = this.head;
 
+      while (current !== null) {
+        if (current.next === this.tail && this.tail !== this.head) {
+          let oldTail = this.tail;
+          current.next = null;
+          this.tail = current;
+          this.length--;
+          return oldTail.val;
+        } else if (this.tail === this.head) {
+          let oldTail = this.tail;
+          this.head = null;
+          this.tail = null;
+          this.length--;
+          return oldTail.val;
+        }
+        current = current.next;
+      }
+    }
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
-
+    if (this.head) { //one
+      let removedEl = this.head;
+      if (this.length === 1) {
+        this.tail = null;
+      }
+      this.head = this.head.next;
+      this.length--;
+      return removedEl.val;
+    }
   }
+
+
 
   /** getAt(idx): get val at idx. */
 
@@ -71,7 +116,7 @@ class LinkedList {
   /** average(): return an average of all values in the list */
 
   average() {
-    
+
   }
 }
 
